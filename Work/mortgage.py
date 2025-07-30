db41331d577473292d05e9ffa0565da300d8c356
Matthias Preskar <matthias.preskar@gmail.com>
2025-07-30 20:13:50 +0200

    commit Überarbeitet Bounce-, Mortgage- und Pcost-Skripte; fügt Sears-Skript hinzu
    	modified:   Work/bounce.py
    	modified:   Work/mortgage.py
    	modified:   Work/pcost.py
    	new file:   Work/sears.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,31 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+current_month = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    current_month += 1
+    
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment    
+        
+    if extra_payment_start_month <= current_month <= extra_payment_end_month:
+        principal -= extra_payment
+        total_paid += extra_payment
+    
+    if principal < 0:
+        total_paid += principal
+        principal = 0        
+    
+    print(f"{current_month:<5} {total_paid:<10.2f} {principal:<10.2f}")    
+
+print("Total paid:", round(total_paid, 2))
+print("Month", current_month)
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolio_cost(filename):
+    "Reads Data out of a give file and sums the portfolio costs"
+    
+    with open(filename, "rt") as f:
+        header = next(f)
+        amount = 0
+        for line in f:
+            row = line.split(",")
+            try:
+                amount += int(row[1]) * float(row[2])
+            except ValueError:
+                print("Value is not present!")
+        return amount
+        
+cost = portfolio_cost("Data/missing.csv")
+print("Total costs:", cost)
