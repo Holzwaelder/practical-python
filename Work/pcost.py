8ce7ed53435c8f8960e9a8727c91a3ac6924abdb
Matthias Preskar <matthias.preskar@gmail.com>
2025-07-30 21:15:22 +0200

    commit

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,19 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
 def portfolio_cost(filename):
     "Reads Data out of a give file and sums the portfolio costs"
     
     with open(filename, "rt") as f:
-        header = next(f)
+        rows = csv.reader(f)
+        header = next(rows)
         amount = 0
-        for line in f:
-            row = line.split(",")
+        for row in rows:            
             try:
                 amount += int(row[1]) * float(row[2])
             except ValueError:
                 print("Value is not present!")
         return amount
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Data/portfolio.csv"
         
-cost = portfolio_cost("Data/missing.csv")
+cost = portfolio_cost(filename)
 print("Total costs:", cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,32 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    "Reads Data out of a given file and returns a list of tuples"
+    
+    portfolio = []
+    
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        header = next(rows)       
+        for row in rows:            
+            try:
+                portfolio.append({"name": row[0], "shares": int(row[1]), "price": float(row[2])})
+            except ValueError:
+                print("Value is not present!")
+        return portfolio
+    
+def read_prices(filename):
+    "Reads prices out of a given file and returns a list of dictionaries"
+    
+    prices = []
+    
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        header = next(rows)       
+        for row in rows:            
+            if bool(row[0]) and bool(row[1]):
+                prices[row[0]] = row[1]                
+        return prices
