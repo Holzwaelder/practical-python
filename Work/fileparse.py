b56d74236b1327c3219022f57c9d72ef443e7cc0
Matthias Preskar <matthias.preskar@gmail.com>
2025-08-04 18:10:29 +0200

    commit 	modified:   Work/fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,10 +3,13 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, select=None, types=None, has_header=True, delimiter=","):
+def parse_csv(filename, select=None, types=None, has_header=True, delimiter=",", silence_errors=False):
     """ 
     Parse a .CSV File into a list of records
     """
+    if select and not has_header:
+        raise RuntimeError("select argument requires column header")
+    
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
                 
@@ -15,22 +18,28 @@ def parse_csv(filename, select=None, types=None, has_header=True, delimiter=",")
             
         # If a column selector was given, find indices of the specified columns
         # Also narrow the set of headers used for resulting dictionaries
-        if select:
+        if select:            
             indices = [header.index(colname) for colname in select]
-            header = select        
-                   
+            header = select 
+                  
         records = []
-        for row in rows:
+        for i, row in enumerate(rows, start=1):
             if not row:     # skip rows with no data
                 continue
             
             # Filter the row if specific columns were selected
-            if indices:
+            if select:
                 row = [row[index] for index in indices]
                 
             # Convert columns to given data types
             if types:
-                row = [func(val) for func, val in zip(types, row)]
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except ValueError as e:
+                    if not silence_errors:
+                        print(f"Row {i} has some missing values {row}")
+                        print(f"Row {i} {e}")
+                    continue
             
             if has_header:
                 # Make a dictionary   
